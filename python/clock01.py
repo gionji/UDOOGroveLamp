639c537e80406807c26458ca5526cd5b98805b8e
gionji <giovanniburresi@gmail.com>
2017-06-15 16:45:14 +0000

    Added python clock test 01

--- python/clock01.py
+++ python/clock01.py
@@ -1,10 +1,22 @@
 # use Tkinter to show a digital clock
 # tested with Python24    vegaseat    10sep2006
+
+#http://www.python-course.eu/tkinter_labels.php
+
 from Tkinter import *
 import time
+from PIL import Image
+
 root = Tk()
+root.attributes("-fullscreen", True)
 time1 = ''
-clock = Label(root, font=('times', 20, 'bold'), bg='green')
+
+image= Image.open("eyesbg.jpg")
+backImg = PhotoImage(image)
+backImgLabel = Label(root, image=backImg)
+backImgLabel.place(x=0,y=0, relwidth=1, relheight=1)
+
+clock = Label(root, font=('verdana', 120, 'bold'), fg='white')
 clock.pack(fill=BOTH, expand=1)
 def tick():
     global time1
